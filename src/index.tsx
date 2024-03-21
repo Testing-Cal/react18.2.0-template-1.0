92648f90b534812eb25f36afc2d9d1fe22f0f994
Yuvraj Madhukar Dhakate <ydhakate@calibo.com>
2024-03-21 16:20:27 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client';
-import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from './home';
-import { Outlet } from "react-router-dom";
 import './style.css';
-import Dashboard  from "./dashboard";
+import Dashboard from "./dashboard";
 import Listing from "./listing";
-import Posts from "./new";
 import PageNotFound from './page-not-found'
-console.log("Inside index.tsx")
-
 
 const App = () => {
   const basePath = process.env.REACT_APP_CONTEXT;
@@ -22,17 +16,10 @@ const App = () => {
               <Link to="/" style={{marginRight: '20px'}}>Home</Link>
               <Link to="/dashboard" style={{marginRight: '20px'}}>Dashboard</Link>
               <Link to="/listing">Listing</Link>
-              <Link to="/dashboard/listing">Listing 1</Link>
-
           </nav>
-
           <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/dashboard/*" element={<Dashboard />} >
-                    <Route index element={<Home />} />
-                    <Route path='new' element={<Posts />} /> {/*A nested route!*/}
-                    <Route path='listing' element={<Listing />} /> {/*A nested route!*/}
-              </Route>
+              <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/listing" element={<Listing />} />
               <Route path="*" element={<PageNotFound />} />
           </Routes>
@@ -41,7 +28,14 @@ const App = () => {
    )
 }
 
-render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error("Root element not found.");
+}
+
 /*
 class based component
 
