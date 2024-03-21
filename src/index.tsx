515e51fe0f40550df08bdb6ffc0eae97d34e29a7
akamble@calibo.com <akamble@calibo.com>
2024-03-21 16:30:46 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from './home';
 import { Outlet } from "react-router-dom";
 import './style.css';
-import Dashboard from "./dashboard";
 import Dashboard  from "./dashboard";
 import Listing from "./listing";
 import Posts from "./new";
@@ -16,6 +15,10 @@ console.log("Inside index.tsx")
 
 const App = () => {
   const basePath = process.env.REACT_APP_CONTEXT;
+  return (
+    <Router basename={basePath}>
+      <div>
+          <nav style={{margin: '20px'}}>
               <Link to="/" style={{marginRight: '20px'}}>Home</Link>
               <Link to="/dashboard" style={{marginRight: '20px'}}>Dashboard</Link>
               <Link to="/listing">Listing</Link>
@@ -25,7 +28,6 @@ const App = () => {
 
           <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/dashboard/*" element={<Dashboard />} >
                     <Route index element={<Home />} />
                     <Route path='new' element={<Posts />} /> {/*A nested route!*/}
@@ -34,7 +36,51 @@ const App = () => {
               <Route path="/listing" element={<Listing />} />
               <Route path="*" element={<PageNotFound />} />
           </Routes>
+      </div>
+    </Router>
    )
+}
+
+render(<App />, document.getElementById('root'));
+/*
+class based component
+
+type AppState = {
+    name: string
+}
+
+type AppProp = {
+
+}
+
+class App extends React.Component<AppProp,AppState> {
+  constructor(props:AppProp) {
+    super(props);
+    this.state = {
+      name: 'Welcome to Integrated Digital Platform'
+    };
+  }
+
+  render() {
+    const basePath = process.env.REACT_APP_CONTEXT;
+      return (
+          <Router basename={basePath}>
+              <div>
+                  <nav style={{margin: '20px'}}>
+                      <Link to="/" style={{marginRight: '20px'}}>Home</Link>
+                      <Link to="/dashboard" style={{marginRight: '20px'}}>Dashboard</Link>
+                      <Link to="/listing">Listing</Link>
+                  </nav>
+                  <Routes>
+                      <Route path="/" element={<Home name={this.state.name}/>} />
+                      <Route path="/dashboard" element={<Dashboard />} />
+                      <Route path="/listing" element={<Listing />} />
+                      <Route path="*" element={<PageNotFound />} />
+                  </Routes>
+              </div>
+          </Router>
+      )
+  }
 }
 
 const rootElement = document.getElementById('root');
@@ -44,7 +90,4 @@ if (rootElement) {
 } else {
   console.error("Root element not found.");
 }
-
-render(<App />, document.getElementById('root'));
-/*
-class based component
+*/
