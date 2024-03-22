0caf16ef9c48a541116bc5b4d3d66f5b2accc188
Yuvraj Madhukar Dhakate <ydhakate@calibo.com>
2024-03-22 05:06:43 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -41,7 +41,15 @@ const App = () => {
    )
 }
 
-render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error("Root element not found.");
+}
+
+
 /*
 class based component
 
