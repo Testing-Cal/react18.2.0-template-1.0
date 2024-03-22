d90af8493a9414d28fe683044f0caef8b832d213
Yuvraj Dhakate <ydhakate@calibo.com>
2024-03-22 13:11:32 +0530

    updated template for nested path

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -1,13 +1,9 @@
-import { Outlet, Link  } from "react-router-dom";
+import * as React from 'react'
 
 function Dashboard(){
     return (
         <div>
-        <h2>This is Dashboard</h2>
-        <nav style={{margin: '20px'}}>
-        <Link to="/dashboard/nested">Nested</Link>
-        </nav>
-        <Outlet />
+          <div>This is Dashbaord Page</div>
         </div>
     );
 }
